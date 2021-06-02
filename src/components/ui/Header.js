57386c43f31e57644d7c2c1e9c025318e943b8e5
Chris Diorio <11452673+PupRiku@users.noreply.github.com>
2021-06-02 14:06:32 -0500

    Add and style tabs

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -3,6 +3,8 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import { makeStyles } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
 import logo from '../../assets/logo.svg';
 
@@ -26,6 +28,17 @@ const useStyles = makeStyles(theme => ({
   },
   logo: {
     height: "7em"
+  },
+  tabContainer: {
+    marginLeft: 'auto'
+  },
+  tab: {
+    fontFamily: "Raleway",
+    textTransform: "none",
+    fontWeight: 700,
+    fontSize: "1rem",
+    minWidth: 10,
+    marginLeft: "25px"
   }
 }));
 
@@ -38,6 +51,13 @@ export default function Header(props) {
             <AppBar position='fixed'>
                 <Toolbar disableGutters>
                   <img src={logo} alt="company logo" className={classes.logo}/>
+                  <Tabs className={classes.tabContainer}>
+                    <Tab className={classes.tab} label="Home" />
+                    <Tab className={classes.tab} label="Services" />
+                    <Tab className={classes.tab} label="The Revolution" />
+                    <Tab className={classes.tab} label="About Us" />
+                    <Tab className={classes.tab} label="Contact Us" />
+                  </Tabs>
                 </Toolbar>
             </AppBar>
         </ElevationScroll>
