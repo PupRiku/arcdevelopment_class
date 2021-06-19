64f4e774e79b36536ff096b665e0aa60281dea77
Chris Diorio <11452673+PupRiku@users.noreply.github.com>
2021-06-18 23:25:36 -0500

    Responsive design adjustments

--- src/components/CustomSoftware.js
+++ src/components/CustomSoftware.js
@@ -6,6 +6,7 @@ import Grid from '@material-ui/core/Grid';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
+import Hidden from '@material-ui/core/Hidden';
 
 import backArrow from '../assets/backArrow.svg';
 import forwardArrow from '../assets/forwardArrow.svg';
@@ -31,6 +32,11 @@ const useStyles = makeStyles((theme) => ({
     paddingRight: '5em',
     paddingTop: '2em',
     paddingBottom: '10em',
+    [theme.breakpoints.down('sm')]: {
+      paddingLeft: '1.5em',
+      paddingRight: '1.5em',
+      paddingTop: '1em',
+    },
   },
   itemContainer: {
     maxWidth: '40em',
@@ -39,6 +45,9 @@ const useStyles = makeStyles((theme) => ({
 
 export default function CustomSoftware(props) {
   const classes = useStyles();
+  const theme = useTheme();
+  const matchesMD = useMediaQuery(theme.breakpoints.down('md'));
+  const matchesSM = useMediaQuery(theme.breakpoints.down('sm'));
 
   const documentsOptions = {
     loop: true,
@@ -78,62 +87,89 @@ export default function CustomSoftware(props) {
 
   return (
     <Grid container direction="column" className={classes.mainContainer}>
-      <Grid item container direction="row">
-        <Grid
-          item
-          className={classes.arrowContainer}
-          style={{ marginRight: '1em', marginLeft: '-3.5em' }}
-        >
-          <IconButton
-            style={{ backgroundColor: 'transparent' }}
-            component={Link}
-            to="/services"
-            onClick={() => props.setSelectedIndex(0)}
+      <Grid
+        item
+        container
+        direction="row"
+        justify={matchesMD ? 'center' : undefined}
+      >
+        <Hidden mdDown>
+          <Grid
+            item
+            className={classes.arrowContainer}
+            style={{ marginRight: '1em', marginLeft: '-3.5em' }}
           >
-            <img src={backArrow} alt="Back to Services Page" />
-          </IconButton>
-        </Grid>
+            <IconButton
+              style={{ backgroundColor: 'transparent' }}
+              component={Link}
+              to="/services"
+              onClick={() => props.setSelectedIndex(0)}
+            >
+              <img src={backArrow} alt="Back to Services Page" />
+            </IconButton>
+          </Grid>
+        </Hidden>
         <Grid item container direction="column" className={classes.heading}>
           <Grid item>
-            <Typography variant="h2">Custom Software Development</Typography>
+            <Typography align={matchesMD ? 'center' : undefined} variant="h2">
+              Custom Software Development
+            </Typography>
           </Grid>
           <Grid item>
-            <Typography variant="body1" paragraph>
+            <Typography
+              align={matchesMD ? 'center' : undefined}
+              variant="body1"
+              paragraph
+            >
               Whether we’re replacing old software or inventing new solutions,
               Arc Development is here to help your business tackle technology.
             </Typography>
-            <Typography variant="body1" paragraph>
+            <Typography
+              align={matchesMD ? 'center' : undefined}
+              variant="body1"
+              paragraph
+            >
               Using regular commercial software leaves you with a lot of stuff
               you don’t need, without some of the stuff you do need, and
               ultimately controls the way you work. Without using any software
               at all you risk falling behind competitors and missing out on huge
               savings from increased efficiency.
             </Typography>
-            <Typography variant="body1" paragraph>
+            <Typography
+              align={matchesMD ? 'center' : undefined}
+              variant="body1"
+              paragraph
+            >
               Our custom solutions are designed from the ground up with your
               needs, wants, and goals at the core. This collaborative process
               produces finely tuned software that is much more effective at
               improving your workflow and reducing costs than generalized
               options.
             </Typography>
-            <Typography variant="body1" paragraph>
+            <Typography
+              align={matchesMD ? 'center' : undefined}
+              variant="body1"
+              paragraph
+            >
               We create exactly what you what, exactly how you want it.
             </Typography>
           </Grid>
         </Grid>
-        <Grid item className={classes.arrowContainer}>
-          <IconButton
-            style={{ backgroundColor: 'transparent' }}
-            component={Link}
-            to="/mobileapps"
-            onClick={() => props.setSelectedIndex(2)}
-          >
-            <img
-              src={forwardArrow}
-              alt="Forward to iOS/Android App Development page"
-            />
-          </IconButton>
-        </Grid>
+        <Hidden mdDown>
+          <Grid item className={classes.arrowContainer}>
+            <IconButton
+              style={{ backgroundColor: 'transparent' }}
+              component={Link}
+              to="/mobileapps"
+              onClick={() => props.setSelectedIndex(2)}
+            >
+              <img
+                src={forwardArrow}
+                alt="Forward to iOS/Android App Development page"
+              />
+            </IconButton>
+          </Grid>
+        </Hidden>
       </Grid>
       <Grid
         item
@@ -163,7 +199,11 @@ export default function CustomSoftware(props) {
           direction="column"
           md
           alignItems="center"
-          style={{ maxWidth: '40em' }}
+          style={{
+            maxWidth: '40em',
+            marginTop: matchesSM ? '10em' : 0,
+            marginBottom: matchesSM ? '10em' : 0,
+          }}
         >
           <Grid item>
             <Typography variant="h4">Save Time</Typography>
@@ -188,22 +228,49 @@ export default function CustomSoftware(props) {
           </Grid>
         </Grid>
       </Grid>
-      <Grid item container direction="row" justify="space-between">
-        <Grid item container className={classes.itemContainer} md>
+      <Grid
+        item
+        container
+        alignItems={matchesMD ? 'center' : undefined}
+        direction={matchesMD ? 'column' : 'row'}
+        justify="space-between"
+      >
+        <Grid
+          item
+          container
+          className={classes.itemContainer}
+          direction={matchesSM ? 'column' : 'row'}
+          md
+          style={{ marginBottom: matchesMD ? '15em' : 0 }}
+        >
           <Grid item container direction="column" md>
             <Grid item>
-              <Typography variant="h4">Digital Documents & Data</Typography>
+              <Typography align={matchesSM ? 'center' : undefined} variant="h4">
+                Digital Documents & Data
+              </Typography>
             </Grid>
             <Grid item>
-              <Typography variant="body1" paragraph>
+              <Typography
+                align={matchesSM ? 'center' : undefined}
+                variant="body1"
+                paragraph
+              >
                 Reduce Errors. Reduce Waste. Reduce Costs.
               </Typography>
-              <Typography variant="body1" paragraph>
+              <Typography
+                align={matchesSM ? 'center' : undefined}
+                variant="body1"
+                paragraph
+              >
                 Billions are spent annually on the purchasing, printing, and
                 distribution of paper. On top of the massive environmental
                 impact this has, it causes harm to your bottom line as well.
               </Typography>
-              <Typography variant="body1" paragraph>
+              <Typography
+                align={matchesSM ? 'center' : undefined}
+                variant="body1"
+                paragraph
+              >
                 By utilizing digital forms and documents you can remove these
                 obsolete expenses, accelerate your communication, and help the
                 Earth.
@@ -217,7 +284,13 @@ export default function CustomSoftware(props) {
             />
           </Grid>
         </Grid>
-        <Grid item container className={classes.itemContainer} md>
+        <Grid
+          item
+          container
+          className={classes.itemContainer}
+          direction={matchesSM ? 'column' : 'row'}
+          md
+        >
           <Grid item md>
             <Lottie
               options={scaleOptions}
@@ -226,12 +299,16 @@ export default function CustomSoftware(props) {
           </Grid>
           <Grid item container direction="column" md>
             <Grid item>
-              <Typography variant="h4" align="right">
+              <Typography variant="h4" align={matchesSM ? 'center' : 'right'}>
                 Scale
               </Typography>
             </Grid>
             <Grid item>
-              <Typography variant="body1" align="right" paragraph>
+              <Typography
+                variant="body1"
+                align={matchesSM ? 'center' : 'right'}
+                paragraph
+              >
                 Whether you’re a large brand, just getting started, or taking
                 off right now, our application architecture ensures pain-free
                 growth and reliability.
@@ -251,8 +328,8 @@ export default function CustomSoftware(props) {
             <img
               src={roots}
               alt="tree with roots extending out"
-              height="450em"
-              width="450em"
+              height={matchesSM ? '300em' : '450em'}
+              width={matchesSM ? '300em' : '450em'}
             />
           </Grid>
           <Grid item className={classes.itemContainer}>
@@ -270,21 +347,49 @@ export default function CustomSoftware(props) {
           </Grid>
         </Grid>
       </Grid>
-      <Grid item container direction="row" justify="space-between">
-        <Grid item container className={classes.itemContainer} md>
+      <Grid
+        item
+        container
+        alignItems={matchesMD ? 'center' : undefined}
+        direction={matchesMD ? 'column' : 'row'}
+        justify="space-between"
+        style={{ marginBottom: '10em' }}
+      >
+        <Grid
+          item
+          container
+          className={classes.itemContainer}
+          direction={matchesSM ? 'column' : 'row'}
+          style={{ marginBottom: matchesMD ? '15em' : 0 }}
+          md
+        >
           <Grid item container direction="column" md>
             <Grid item>
-              <Typography variant="h4">Automation</Typography>
+              <Typography align={matchesSM ? 'center' : undefined} variant="h4">
+                Automation
+              </Typography>
             </Grid>
             <Grid item>
-              <Typography variant="body1" paragraph>
+              <Typography
+                align={matchesSM ? 'center' : undefined}
+                variant="body1"
+                paragraph
+              >
                 Why waste time when you don’t have to?
               </Typography>
-              <Typography variant="body1" paragraph>
+              <Typography
+                align={matchesSM ? 'center' : undefined}
+                variant="body1"
+                paragraph
+              >
                 We can help you identify processes with time or event based
                 actions which can now easily be automated.
               </Typography>
-              <Typography variant="body1" paragraph>
+              <Typography
+                align={matchesSM ? 'center' : undefined}
+                variant="body1"
+                paragraph
+              >
                 Increasing efficiency increases profits, leaving you more time
                 to focus on your business, not busywork
               </Typography>
@@ -297,7 +402,13 @@ export default function CustomSoftware(props) {
             />
           </Grid>
         </Grid>
-        <Grid item container className={classes.itemContainer} md>
+        <Grid
+          item
+          container
+          className={classes.itemContainer}
+          direction={matchesSM ? 'column' : 'row'}
+          md
+        >
           <Grid item md>
             <Lottie
               options={uxOptions}
@@ -306,19 +417,31 @@ export default function CustomSoftware(props) {
           </Grid>
           <Grid item container direction="column" md>
             <Grid item>
-              <Typography variant="h4" align="right">
+              <Typography variant="h4" align={matchesSM ? 'center' : 'right'}>
                 User Experience Design
               </Typography>
             </Grid>
             <Grid item>
-              <Typography variant="body1" align="right" paragraph>
+              <Typography
+                variant="body1"
+                align={matchesSM ? 'center' : 'right'}
+                paragraph
+              >
                 A good design that isn’t usable isn’t a good design.
               </Typography>
-              <Typography variant="body1" align="right" paragraph>
+              <Typography
+                variant="body1"
+                align={matchesSM ? 'center' : 'right'}
+                paragraph
+              >
                 So why are so many pieces of software complicated, confusing,
                 and frustrating?
               </Typography>
-              <Typography variant="body1" align="right" paragraph>
+              <Typography
+                variant="body1"
+                align={matchesSM ? 'center' : 'right'}
+                paragraph
+              >
                 By prioritizing users and the real ways they interact with
                 technology we’re able to develop unique, personable experiences
                 that solve problems rather than create new ones.
